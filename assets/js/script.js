4050f50120dc546b2f28d73ba07a2ddfc986ee2d
Maiara Santos <maiarasantos34@yahoo.com>
2023-10-17 14:20:28 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -213,11 +213,3 @@ window.addEventListener('load', function () {
   displayCurrentDateInCards();
 });
 
-// Função para zerar os números de visualizações no localStorage
-function resetViewCounts() {
-  localStorage.removeItem('viewCounts');
-}
-
-// Chama a função para zerar as visualizações (pode ser chamada quando necessário)
-resetViewCounts();
-
