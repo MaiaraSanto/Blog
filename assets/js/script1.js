fc5a604fd773e1177c6d069a319264475490cdbe
Maiara Santos <maiarasantos34@yahoo.com>
2023-10-17 13:56:25 -0300

    Update script1.js

--- assets/js/script1.js
+++ assets/js/script1.js
@@ -154,4 +154,12 @@ if (theme) {
     }
 }
 
+// Função para zerar os números de visualizações no localStorage
+function resetViewCounts() {
+  localStorage.removeItem('viewCounts');
+}
+
+// Chama a função para zerar as visualizações (pode ser chamada quando necessário)
+resetViewCounts();
+
 
